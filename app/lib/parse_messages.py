4ffdf313f445b9c161022afb2d542032fc3ea78d
AlexanderPease <zander@nomadhealth.com>
2019-04-04 16:56:03 -0400

    Rm comments

--- app/lib/parse_messages.py
+++ app/lib/parse_messages.py
@@ -24,45 +24,7 @@ def parse_message(message):
                     email_str=email_str,
                     action=action,
                     name=name_str)
-
-
-        # if name == 'from':
-        #     email_address = EmailAddress.get_or_create(email_str)
-        #     if not message._from_email:
-        #         message._from_email = email_address
-        #         db.session.add(message)
-        #         db.session.commit()
-
-        #     # Can only be a single From: per message
-        #     # kwargs = dict(message_id=message.id, action='from')
-        #     # try:
-        #     #     MessageEmailAddress.query.filter_by(**kwargs).one()
-        #     # except NoResultFound:
-        #     #     new_from = MessageEmailAddress(
-        #     #         email_id=email_address.id, **kwargs
-        #     #     )
-        #     #     db.session.add(new_from)
-        #     #     db.session.commit()
-
-        # elif name == 'to':
-        #     # To: is a string of comma-delimited addresses
-        #     addresses = value.split(', ')
-        #     for address in addresses:
-        #         name_str, email_str = parseaddr(address)
-        #         email_address = EmailAddress.get_or_create(email_str)
-        #         message.add_to_email(email_address)
-
-        # elif name == 'cc':
-        #     pass
-        # elif name == 'bcc':
-        #     pass
-        # elif name == 'delivered-to':
-        #     pass
         elif action == 'date':
             pass
         elif action == 'subject':
             pass
-
-
-# def parse_from(value):
-#     return parseaddr(value)
