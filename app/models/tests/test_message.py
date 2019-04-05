ee448ead7e76616e7f770204c3c2220c03b92595
AlexanderPease <zander@nomadhealth.com>
2019-04-05 00:11:10 -0400

    Basic nosetest setup. Need to work inside app context

---
+++ app/models/tests/test_message.py
@@ -0,0 +1,17 @@
+import requests
+import os
+import time
+from nose.tools import assert_equals
+
+
+class TestMessage:
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def setup(self):
+        pass
+
+    def test_basic(self):
+        assert_equals(2, 2)
