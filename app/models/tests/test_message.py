ae3001e25d7a8b9366acf250333da770c0a8e0b8
AlexanderPease <zander@nomadhealth.com>
2019-04-05 00:26:52 -0400

    WIP flask app context for nose

--- app/models/tests/test_message.py
+++ app/models/tests/test_message.py
@@ -3,15 +3,34 @@ import os
 import time
 from nose.tools import assert_equals
 
+from app import app
+from app.models import db
+from app.models.message import Message, EmailAddress
+
 
 class TestMessage:
 
     @classmethod
     def setup_class(cls):
-        pass
+        # Gets non-.env config vars
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/crm-test'
+        # print(app.config)
+        
+
+        with app.app_context():
+            time.sleep(2)
+
+
 
     def setup(self):
         pass
 
-    def test_basic(self):
-        assert_equals(2, 2)
+    def test_init(self):
+        message = Message(thread_id=1)
+        assert_equals(message.thread_id, 1)
+        
+        db.session.add(message)
+        db.session.commit()
+        assert_equals(
+            len(Message.query.all()), 1
+        )
